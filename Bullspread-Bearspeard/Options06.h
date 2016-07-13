a28121a66dae896b4fa9cd4d88a14f3860dc6eeb
ccc639 <ccc639@nyu.edu>
2016-07-13 16:19:02 -0400

    Add files via upload

---
+++ Bullspread-Bearspeard/Options06.h
@@ -0,0 +1,63 @@
+#ifndef Options06_h
+#define Options06_h
+#include "BinModel02.h"
+
+class EurOption
+{
+private:
+	int N; //steps to expiry
+public:
+	void SetN(int N_) { N = N_; }
+	//Payoff defined to return 0.0
+	//for pedagogical purposes.
+	//To use a pure virtual function replace by
+	//virtual double Payoff(double z)=0;
+	virtual double Payoff(double z) { return 0.0; }
+	//pricing European option
+	double PriceByCRR(BinModel Model);
+};
+
+class Call : public EurOption
+{
+private:
+	double K; //strike price
+public:
+	void SetK(double K_) { K = K_; }
+	int GetInputData();
+	double Payoff(double z);
+};
+
+class Put : public EurOption
+{
+private:
+	double K; //strike price
+public:
+	void SetK(double K_) { K = K_; }
+	int GetInputData();
+	double Payoff(double z);
+};
+
+class BullSpread : public EurOption
+{
+private:
+	double K1, K2; // parameter 1 & parameter 2
+public:
+	void SetK1(double K1_) { K1 = K1_; }
+	void SetK2(double K2_) { K2 = K2_; }
+	int GetInputData();
+	double Payoff(double z);
+};
+
+class BearSpread : public EurOption
+{
+private:
+	double K1, K2; // parameter 1 & parameter 2
+public:
+	void SetK1(double K1_) { K1 = K1_; }
+	void SetK2(double K2_) { K2 = K2_; }
+	int GetInputData();
+	double Payoff(double z);
+};
+
+#endif
+
